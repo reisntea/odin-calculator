79f5b4df1e38e71323487df06bbf88bd8ebfbb9f
Brian C <283823263+reisntea@users.noreply.github.com>
2026-06-10 20:42:43 -0400

    Add functionality to number and clear buttons
    Clicking on the numbers now changes the display. The code also correctly
    handles if the display is 0 or empty. Had to adjust html and css so that
    the display wouldn't shrink when empty.

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-let numA = 0;
-let numB = 0;
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector("#ac");
+const display = document.querySelector("#display")
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.id);
+        updateDisplay(button.id);
+    });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.id);
+    });
+});
+
+clearButton.addEventListener("click", () => {
+    console.log(clearButton.id);
+    clearDisplay();
+});
+
+function updateDisplay (number) {
+    if (display.textContent == 0 || display.textContent == "") {
+        display.textContent = number
+    } else {
+        display.textContent += number;
+    }
+}
+
+function clearDisplay () {
+    display.textContent = '';
+}
+
+let numA;
+let numB;
 let operator = ``;
 
 function add (a, b) {
@@ -32,4 +67,3 @@ function operate (a, b, operator) {
         return "Could not calculate.";
     }
 }
-
